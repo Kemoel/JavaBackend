9199a12644819b632a64bccbda0e14f6171500b7
Kemoel <93206779+Kemoel@users.noreply.github.com>
2023-03-30 17:36:28 -0600

    first commit

---
+++ src/main/java/com/webapp/webapp/model/Person.java
@@ -0,0 +1,28 @@
+package com.webapp.webapp.model;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class Person {
+
+    private final UUID id;
+
+    @NotBlank
+    private final String name;
+
+    public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public  UUID getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
